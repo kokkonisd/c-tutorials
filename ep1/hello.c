c18e300c88d00d1b284f0fa30e0d23b356b7a2e8
Dimitris Kokkonis <kokkonisd@gmail.com>
2019-08-13 17:36:31 +0200

    Add code for the first episode

---
+++ ep1/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// τύπος όνομα (παράμετρος1, παράμετρος2, ...)
+int main (int argc, char * argv[]) // def main(argc, argv):
+{
+	printf("Hello, World!\n"); 	   //     print("Hello, World!")
+
+	return 0;                      //     return 0
+}
